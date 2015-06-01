2a0b166556bc413667af29d42ab670269ff323f8
Lismine57 <lismine57@gmail.com>
2015-06-01 17:15:40 +0800

    new CalculatorView

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -5,6 +5,8 @@
  */
 package tw.edu.npu.mis;
 
+import javax.swing.JFrame;
+
 /**
  * The model class of the calculator application.
  */
@@ -54,7 +56,9 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new CalculatorView().setVisible(true);
+    
+        
     }
 
 }
